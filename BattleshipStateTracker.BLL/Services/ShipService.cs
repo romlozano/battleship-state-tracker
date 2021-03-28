1babeef59916cb9f3912a3c0b107f10d4104dec8
romlozano <romlozano@gmail.com>
2021-03-28 17:21:58 +1100

    Pass AttackShip test

--- BattleshipStateTracker.BLL/Services/ShipService.cs
+++ BattleshipStateTracker.BLL/Services/ShipService.cs
@@ -203,7 +203,7 @@ namespace BattleshipStateTracker.BLL.Services
                 }
             }
 
-            return AttackShipResultEnum.Sunk;
+            return AttackShipResultEnum.Miss;
         }
     }
 }
